5c3cc6fc445bf2a3bc6b2b76fe7a293fbcc1f565
gnuhack <gnuhack@github.com/gnuhack>
2020-05-12 17:55:02 +0200

    Eliminada la carpeta Microelec

--- cont_digito.vhd
+++ cont_digito.vhd
@@ -18,11 +18,20 @@ begin
 
   cuenta <= std_logic_vector(salida);
 
-  comb: process(salida)
+  comb: process(salida,enable)
   begin
+    p_salida <= salida;
+    sat <='0';
+    
+    if (enable = '1') then
       p_salida <= salida + 1;
+      if (salida="1001") then
+        sat <= '1';
+        p_salida <= "0000";
+      end if;
+    end if;
+    
   end process;
-  salida <= p_salida;
 
   sinc: process(rst, clk)
   begin
@@ -31,6 +40,7 @@ begin
     elsif (rising_edge(clk)) then
       salida <= p_salida;
     end if;
+
   end process;
 
 end Behavioral;
