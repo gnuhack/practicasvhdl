81ef73a53ecdfc2bd17bd64273d721f09924be85
gnuhack <carlospajuelo_@hotmail.com>
2020-10-08 19:07:21 +0200

    Práctica terminada

---
+++ ETSI2021/Microelectrónica/P2/comparador.vhd
@@ -0,0 +1,61 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+entity comparador is
+  generic (Nbit: integer:= 10;
+           End_Of_Screen: integer:= 10;
+           Start_Of_Pulse: integer:= 20;
+           End_Of_Pulse: integer:= 30;
+           End_Of_Line: integer:= 40
+           );
+  port (reset : in std_logic;
+        clk     : in std_logic;
+        data    : in std_logic_vector(Nbit-1 downto 0);
+        O1      : out std_logic;
+        O2      : out std_logic;
+        O3      : out std_logic
+        );
+  end comparador;
+
+  architecture Behavioral of comparador is
+    
+    signal p_o1, p_o2, p_o3 : std_logic;
+
+  begin
+    
+    comb : process (data)
+    begin
+      if (To_integer(unsigned(data)) > End_Of_Screen)  then
+        p_o1 <='1';
+      else
+        p_o1 <='0';
+      end if;
+
+      if (Start_Of_Pulse < To_integer(unsigned(data))  and To_integer(unsigned(data)) < End_Of_Pulse) then
+        p_o2 <='1';
+      else
+        p_o2 <='0';
+      end if;
+
+      if (To_integer(unsigned(data)) = End_Of_Line) then
+        p_o3 <='1';
+      else
+        p_o3 <='0';
+      end if;
+      
+      end process;
+      sinc : process(reset,p_o1,p_o2,p_o3)
+      begin
+        if (reset = '1') then
+          O1<='0';
+          O2<='0';
+          O3<='0';
+        elsif (reset = '0') then
+          O1 <= p_o1;
+          O2 <= p_o2;
+          O3 <= p_o3;
+          end if;
+        end process;
+          
+    end Behavioral;
---
+++ ETSI2021/Microelectrónica/P2/dibuja.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity dibuja is
+  Port ( eje_x : in STD_LOGIC_VECTOR (9 downto 0);
+         eje_y : in STD_LOGIC_VECTOR (9 downto 0);
+         R : out STD_LOGIC;
+         G : out STD_LOGIC;
+         B : out STD_LOGIC);
+end dibuja;
+
+architecture Behavorial of dibuja is
+  
+begin
+
+  process(eje_x, eje_y)
+  begin
+      G<='1'; B<='1'; R<='1';
+    if ((To_integer(Unsigned(eje_x))>279 and To_integer(Unsigned(eje_x))<359) or (To_integer(Unsigned(eje_y))>209 and To_integer(Unsigned(eje_y))<269)) then
+      R<='1';G<='0';B<='0';
+    end if;
+
+end process;
+
+
+end Behavorial;
+  
