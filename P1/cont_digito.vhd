3e0a9cab5a88cb204eb9c3041890b79d75d2ae9d
gnuhack <carlospajuelo_@hotmail.com>
2021-03-16 00:18:28 +0100

    Terminado el fichero cont_digito, funciona correctamente

--- P1/cont_digito.vhd
+++ P1/cont_digito.vhd
@@ -11,30 +11,30 @@ end cont_digito;
 
 architecture Behavioral of cont_digito is
 -- entradas
-  signal p_cuenta : unsigned (3 downto 0);
-  signal cuentap  : unsigned(3 downto 0);  --:="0000";
-  signal debug    : unsigned(1 downto 0) := "01";
+  signal cont : unsigned (3 downto 0);
+  signal p_cont  : unsigned(3 downto 0):="0000";
+ -- signal debug    : unsigned(1 downto 0) := "01";
 begin
-  --cuenta <= cuentap;
-  comb : process(enable, cuentap)
+  cuenta <= std_logic_vector(cont);
+  comb : process(enable, cont)
   begin
 --p_cuenta <= unsigned(cuentap);
     if (enable = '1') then
-      p_cuenta <= cuentap+1;
-      debug    <= debug + 1;
+      p_cont <= cont+1;
+     -- debug    <= debug + 1;
     else
-      p_cuenta <= cuentap;
+      p_cont <= cont;
     end if;
   end process;
 
   sinc : process(reset, clk)
   begin
     if (reset = '1') then
-      cuenta  <= (others => '0');
-      cuentap <= (others => '0');
-      debug   <= (others => '0');
-    elsif (rising_edge(clk)) then
-      cuenta <= std_logic_vector(p_cuenta);
+      cont  <= (others => '0');
+    --  cuentap <= (others => '0');
+     -- debug   <= (others => '0');
+    elsif(rising_edge(clk)) then
+      cont <= p_cont;
     end if;
 
   end process;
